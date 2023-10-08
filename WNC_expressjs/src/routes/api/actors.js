14e006f202c15036aa06d0e0c3ca886d45e091ed
lienvanduc1808 <20127467@student.hcmus.edu.vn>
2023-10-08 19:15:36 +0700

    Completed put actor api

--- WNC_expressjs/src/routes/api/actors.js
+++ WNC_expressjs/src/routes/api/actors.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { DeleteAnActor, GetActors } from "../../db/actors.js";
+import { DeleteAnActor, GetActors, UpdateAnActor } from "../../db/actors.js";
 import { CallAndCatchAsync } from "../../utils/utils.js";
 import { z } from "zod";
 
@@ -20,6 +20,19 @@ const ActorPatchSchema = z
       return !!last_name || !!first_name;
     },
     { message: "At least 1 value" }
+  );
+
+const ActorPutSchema = z
+  .object({
+    last_name: z.string().optional(),
+    first_name: z.string().optional(),
+  })
+  .partial()
+  .refine(
+    function ({ last_name, first_name }) {
+      return !!last_name && !!first_name;
+    },
+    { message: "incomplete value" }
   );
 
 actors_router.get("/", async function (req, res) {
@@ -47,9 +60,31 @@ actors_router.get("/:id", async function (req, res) {
   const { id } = req.params;
   return res.status(200).json({ Hello: id });
 });
-actors_router.put("/:id");
+actors_router.put("/:id", async function (req, res) {
+  const { id } = req.params;
+  const [info, q_err] = await CallAndCatchAsync(
+    ActorPutSchema.parseAsync,
+    req.body
+  );
+  if (q_err != null) {
+    return res.status(400).json({ error: "Invalid params" });
+  }
+
+  if (!id) {
+    return res.status(400).json({ error: "Missing id!" });
+  }
+
+  const [data, err] = await CallAndCatchAsync(UpdateAnActor, { id, info });
+
+  if (err) {
+    return res.status(500).json({ msg: "Server error!", error: err });
+  }
+
+  return res.status(200).json(data);
+});
 actors_router.delete("/:id", async function (req, res) {
   const { id } = req.params;
+  console.log("id", id);
 
   if (!id) {
     return res.status(400).json({ error: "Missing id!" });
