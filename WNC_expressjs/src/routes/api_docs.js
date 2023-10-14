9d8d6f436666be6de387f494cc995736dcf08b3c
Lamlance <lamhoangdien113@gmail.com>
2023-10-14 20:49:20 +0700

    commponent error response

--- WNC_expressjs/src/routes/api_docs.js
+++ WNC_expressjs/src/routes/api_docs.js
@@ -7,47 +7,67 @@ import { ActorSchema } from "../db/actors.js";
 import { FilmSchema } from "../db/films.js";
 import APIPath from "./api/api_path.json" assert { type: "json" };
 const jsonSchema = {
-    openapi: "3.1.0",
-    components: {
-        schemas: {
-            Actor: zodToJsonSchema(ActorSchema, {
-                target: "openApi3",
-            }),
-            Film: zodToJsonSchema(FilmSchema, {
-                target: "openApi3",
-            }),
-            Error: {
-                type: "object",
-                properties: {
-                    error: { type: "string" },
-                },
-            },
+  openapi: "3.1.0",
+  components: {
+    schemas: {
+      Actor: zodToJsonSchema(ActorSchema, {
+        target: "openApi3",
+      }),
+      Film: zodToJsonSchema(FilmSchema, {
+        target: "openApi3",
+      }),
+      Error: {
+        type: "object",
+        properties: {
+          error: { type: "string" },
         },
+      },
     },
+    responses: {
+      "400InvalidInput": {
+        content: {
+          "application/json": {
+            schema: {
+              $ref: "#/components/schemas/Error",
+            },
+          },
+        },
+      },
+      "500ServerError": {
+        content: {
+          "application/json": {
+            schema: {
+              $ref: "#/components/schemas/Error",
+            },
+          },
+        },
+      },
+    },
+  },
 };
 const options = {
-    definition: {
-        openapi: "3.1.0",
-        info: {
-            title: "Express REST APIs",
-            version: "0.1.0",
-            description: "Advance web REST APIs",
-            license: {
-                name: "MIT",
-                url: "https://spdx.org/licenses/MIT.html",
-            },
-        },
-        servers: [
-            {
-                url: "http://localhost:3085/api",
-            },
-        ],
-        ...jsonSchema,
-        paths: {
-            ...APIPath,
-        },
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Express REST APIs",
+      version: "0.1.0",
+      description: "Advance web REST APIs",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
     },
-    apis: ["./src/routes/api/*.js"],
+    servers: [
+      {
+        url: "http://localhost:3085/api",
+      },
+    ],
+    ...jsonSchema,
+    paths: {
+      ...APIPath,
+    },
+  },
+  apis: ["./src/routes/api/*.js"],
 };
 
 const specs = swaggerJsdoc(options);
