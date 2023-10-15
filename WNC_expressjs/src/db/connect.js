8f6ae249f839994429e8ae78dbe54f30be2e9ade
An Tran Van <86640630+antrvan746@users.noreply.github.com>
2023-10-15 23:14:54 +0700

    Restore db config

--- WNC_expressjs/src/db/connect.js
+++ WNC_expressjs/src/db/connect.js
@@ -6,8 +6,8 @@ const db_connection = knex({
     host: "127.0.0.1",
     port: 3666,
     user: "root",
-    password: "123456",
-    database: "sakila_db",
+    password: "mysql",
+    database: "actor",
   },
 });
 
