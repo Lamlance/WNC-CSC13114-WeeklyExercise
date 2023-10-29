c330484605ea7f2f55cbbdeb32229ae3dc374fcb
Lamlance <lamhoangdien113@gmail.com>
2023-10-29 16:25:25 +0700

    log to rabbitmq

--- WNC_expressjs/logger.js
+++ WNC_expressjs/logger.js
@@ -1,5 +1,6 @@
 import { createLogger, format, transports } from "winston";
 import { default as pm2_io } from "@pm2/io";
+import RabbitMQLogger from "./src/logger/RabbitMQLogger.js";
 
 const logger = createLogger({
   level: "debug",
@@ -10,6 +11,7 @@ const logger = createLogger({
   transports: [
     new transports.Console({ consoleWarnLevels: ["error"], level: "error" }),
     new transports.File({ filename: "./logs/error.log", level: "error" }),
+    new RabbitMQLogger({ level: "error" }),
   ],
 });
 
---
+++ WNC_expressjs/src/logger/RabbitMQLogger.js
@@ -0,0 +1,50 @@
+import Transport from "winston-transport";
+import amqp from "amqplib/callback_api.js";
+
+/**
+ * @class
+ * @extends Transport
+ */
+class RabbitMQLogger extends Transport {
+  /**
+   * @type {amqp.Channel | null}
+   */
+  rabbitmq_channel = null;
+  /**
+   * @param {Transport.TransportStreamOptions | undefined} opts
+   */
+  constructor(opts) {
+    super(opts);
+    this.rabbitmq_ch_callback = this.rabbitmq_ch_callback.bind(this);
+    const rabbitmq_ch_callback = this.rabbitmq_ch_callback;
+
+    amqp.connect("amqp://guest:guest@localhost:8672", function (err, conn) {
+      conn.createChannel(rabbitmq_ch_callback);
+    });
+  }
+
+  /**
+   * @param {any} err
+   * @param {amqp.Channel} ch
+   */
+  rabbitmq_ch_callback(err, ch) {
+    this.rabbitmq_channel = ch;
+    ch.assertQueue("log-messages", { durable: true });
+  }
+
+  log(info, callback) {
+    setImmediate(() => {
+      this.emit("logged", info);
+    });
+    if (typeof info === "object") {
+      this.rabbitmq_channel.sendToQueue(
+        "log-messages",
+        Buffer.from(JSON.stringify(info))
+      );
+    } else if (typeof info === "string") {
+      ch.sendToQueue("log-messages", Buffer.from(info));
+    }
+    callback();
+  }
+}
+export default RabbitMQLogger;
