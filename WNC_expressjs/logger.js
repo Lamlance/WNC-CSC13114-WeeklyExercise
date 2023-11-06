ac93f5b0c304ae5e489be816f6c46d235942f163
anhzo12 <20127439clc1@gmail.com>
2023-11-07 00:48:56 +0700

    fix conflict

--- WNC_expressjs/logger.js
+++ WNC_expressjs/logger.js
@@ -9,18 +9,18 @@ const { MongoDB } = pkg;
 import "winston-syslog";
 import os from "os";
 
-const papertrail = new transports.Syslog({
-  host: 'logs4.papertrailapp.com',
-  port: 16512,
-  protocol: 'tls4',
-  localhost: os.hostname(),
-  eol: '\n',
-});
-const minuteTransport = new DailyRotateFile({
-  filename: "./logs/%DATE%/application-%DATE%.log",
-  frequency: "m",
-  datePattern: "YYYY-MM-DD-HH-mm",
-});
+// const papertrail = new transports.Syslog({
+//   host: 'logs4.papertrailapp.com',
+//   port: 16512,
+//   protocol: 'tls4',
+//   localhost: os.hostname(),
+//   eol: '\n',
+// });
+// const minuteTransport = new DailyRotateFile({
+//   filename: "./logs/%DATE%/application-%DATE%.log",
+//   frequency: "m",
+//   datePattern: "YYYY-MM-DD-HH-mm",
+// });
 
 // const dailyTransport = new DailyRotateFile({
 //   filename: "./logs/%DATE%/application-%DATE%.log",
@@ -46,24 +46,24 @@ const logger = createLogger({
   format: format.combine(format.json()),
   transports: [
     new transports.Console({ consoleWarnLevels: ["error"], level: "error" }),
-    new transports.File({ filename: "./logs/error.log", level: "error" }),
-    new transports.File({ filename: "./logs/combined.log" }),
-    new DailyRotateFile({
-      filename: "./logs/%DATE%/application-%DATE%.log",
-      datePattern: "YYYY-MM-DD",
-      zippedArchive: true,
-      maxSize: "2m",
-      maxFiles: "14d",
-    }),
-    new MongoDB({
-      db: "mongodb+srv://", // mongodb uri
-      level: "info",
-      collection: "logs",
-      options: { useUnifiedTopology: true },
-    }),
-    minuteTransport,
-    dailyTransport,
-    papertrail,
+    // new transports.File({ filename: "./logs/error.log", level: "error" }),
+    // new transports.File({ filename: "./logs/combined.log" }),
+    // new DailyRotateFile({
+    //   filename: "./logs/%DATE%/application-%DATE%.log",
+    //   datePattern: "YYYY-MM-DD",
+    //   zippedArchive: true,
+    //   maxSize: "2m",
+    //   maxFiles: "14d",
+    // }),
+    // new MongoDB({
+    //   db: "mongodb+srv://", // mongodb uri
+    //   level: "info",
+    //   collection: "logs",
+    //   options: { useUnifiedTopology: true },
+    // }),
+    // minuteTransport,
+    // dailyTransport,
+    // papertrail,
   ],
 });
 export { logger as WinstonLogger };
